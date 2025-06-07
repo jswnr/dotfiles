ee317c90760e498f20f5e469d8599900201da291
jswnr <215173239+jswnr@users.noreply.github.com>
2025-06-07 21:08:00 +0200

    upload config files

---
+++ .config/nvim/lua/base.lua
@@ -0,0 +1,12 @@
+local o = vim.o
+
+o.number = true
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.smartindent = true
+o.wrap = true
+o.hlsearch = true
+o.incsearch = true
+o.termguicolors = true
+o.clipboard = 'unnamedplus'
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,19 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+Plug('navarasu/onedark.nvim')
+Plug('windwp/nvim-autopairs')
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-vsnip')
+Plug('hrsh7th/vim-vsnip')
+vim.call('plug#end')
+
+vim.cmd('silent! colorscheme onedark')
+
+require('nvim-autopairs').setup {}
